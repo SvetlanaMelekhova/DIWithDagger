0f6706ef0011c1cfe459e84f9ac73a05784a470b
SvetlanaMelekhova <s.melekhova@dinamicadigital.io>
2022-04-27 16:54:12 +0300

    lesson 4

--- app/src/main/java/com/svetlana/learn/diwithdagger/example2/data/network/ExampleApiService.kt
+++ app/src/main/java/com/svetlana/learn/diwithdagger/example2/data/network/ExampleApiService.kt
@@ -1,12 +1,16 @@
 package com.svetlana.learn.diwithdagger.example2.data.network
 
+import android.content.Context
 import android.util.Log
+import com.svetlana.learn.diwithdagger.R
 import javax.inject.Inject
 
-class ExampleApiService @Inject constructor() {
+class ExampleApiService @Inject constructor(
+    private val context: Context
+) {
 
     fun method() {
-        Log.d(LOG_TAG, "ExampleApiService")
+        Log.d(LOG_TAG, "ExampleApiService ${context.getString(R.string.app_name)}")
     }
 
     companion object{
--- app/src/main/java/com/svetlana/learn/diwithdagger/example2/presentation/MainActivity.kt
+++ app/src/main/java/com/svetlana/learn/diwithdagger/example2/presentation/MainActivity.kt
@@ -3,6 +3,7 @@ package com.svetlana.learn.diwithdagger.example2.presentation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.svetlana.learn.diwithdagger.R
+import com.svetlana.learn.diwithdagger.example2.di.ContextModule
 import com.svetlana.learn.diwithdagger.example2.di.DaggerAppComponent
 import javax.inject.Inject
 
@@ -11,13 +12,16 @@ class MainActivity : AppCompatActivity() {
     @Inject
     lateinit var viewModel: ExampleViewModel
 
-    private val component = DaggerAppComponent.create()
+    private val component by lazy {
+        DaggerAppComponent.builder()
+            .contextModule(ContextModule(application))
+            .build()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         component.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         viewModel.method()
-        //DaggerAppComponent.create().inject(this)
     }
 }
